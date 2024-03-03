6eb5c880ef11a789f095f71c129d31db5c5f866b
gregcseszneg <cseszneg.gergo09@gmail.com>
2024-03-03 14:43:37 +0100

    updated API for new login fields

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,29 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
+        [StringLength(16, MinimumLength = 4)]
         public string UserName { get; set; }
+
         [Required]
-        [StringLength(8, MinimumLength = 4)]
+        [StringLength(12)]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public DateOnly? DateOfBirth { get; set; } //optional to make required work!
+        
+        [Required]
+        [StringLength(64)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 5)]
         public string Password { get; set; }
     }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,6 +14,7 @@ namespace API.Helpers
             .ForMember (dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));  //mapping dob.calculateAge to memberDto age field
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
