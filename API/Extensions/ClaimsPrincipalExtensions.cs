27a5a75617f5aaed851af7f75d5c7425652f6f39
gregcseszneg <cseszneg.gergo09@gmail.com>
2024-03-12 22:24:05 +0100

    optimized LogUserActivity

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -4,9 +4,14 @@ namespace API.Extensions
 {
     public static class ClaimsPrincipalExtensions
     {
-        public static string GetUsername(this ClaimsPrincipal user)
+        public static string GetUsername(this ClaimsPrincipal user) //extending claimPrincipal user obejct
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value; //unique name comes in as "Name"
+        }
+
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value; //unique name comes in as "Name"
         }
     }
 }
