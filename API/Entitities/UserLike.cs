1f527d9ad451f14b4633f65791939df6060fccea
gregcseszneg <cseszneg.gergo09@gmail.com>
2024-03-25 18:23:59 +0100

    added likes entity

---
+++ API/Entitities/UserLike.cs
@@ -0,0 +1,11 @@
+namespace API.Entitities
+{
+    //join table
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+        public int SourceUserId { get; set; }
+        public AppUser TargetUser { get; set; }
+        public int TargetUserId { get; set; }
+    }
+}
